80226ee6e99633f920361761fd7e95cd123c0d9c
whmnb666 <77133158+whmnb666@users.noreply.github.com>
2026-07-08 13:33:41 +0800

    Update build-miui.sh

--- build-miui.sh
+++ build-miui.sh
@@ -230,6 +230,7 @@ scripts/config --file out/.config \
     -e BINDER_OPT \
     -e KPERFEVENTS \
     -e MILLET \
+    -d PERF_HUMANTASK \
     -d LTO_CLANG \
     -e LTO_NONE \
     -e SF_BINDER \
